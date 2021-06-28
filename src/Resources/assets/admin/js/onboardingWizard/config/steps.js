41f59ed796e682847ba8c426bb2358489370ea0a
Harvel218 <gacekrzysztof.218@gmail.com>
2021-06-28 11:39:52 +0200

    PR feedback - gulp and rollup deletion

--- src/Resources/assets/admin/js/onboardingWizard/config/steps.js
+++ src/Resources/assets/admin/js/onboardingWizard/config/steps.js
@@ -265,7 +265,7 @@ export const steps = [
 		highlightClass: 'payment-settings',
 		btnNextClass: 'shepherd-button--arrow-down',
 		attachTo: {
-			element: '.js-countryRestrictions',
+			element: '.js-onboardingWizard-countryRestrictions',
 			on: 'top-start',
 		},
 	},
